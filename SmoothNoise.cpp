7a8fff9087b26f0fb705cd27ef378f0a20a626e6
Anton Agestam <msn@antonagestam.se>
2016-12-29 16:36:49 +0100

    initial commit

---
+++ SmoothNoise.cpp
@@ -0,0 +1,40 @@
+#include "SmoothNoise.h"
+
+float cubic(float n) {
+    n = 2 * n;
+    if (n < 1)
+        return .5 * pow(n, 3);
+    n = n - 2;
+    return .5 * (pow(n, 3) + 2);
+}
+
+float cubic_interpolate(float n, float a, float b, float c, float d) {
+    /**
+     * Maps a value n from the range (a, b) to the range (c, d)
+     */
+    float r = 1.0 * (n - a) / (b - a);
+    return cubic(r) * (d - c) + c;
+}
+
+
+float R () {
+    return random(100000) / 100000.0;
+}
+
+SmoothNoise::SmoothNoise(int num_steps) {
+    steps = num_steps;
+    steps_from_last = random(steps);
+    next = R();
+    last = R();
+}
+
+float SmoothNoise::noise(void) {
+    if(steps_from_last == steps) {
+        steps_from_last = 0;
+        last = next;
+        next = R();
+    }
+    float current = cubic_interpolate(steps_from_last, 0, steps, last, next);
+    steps_from_last++;
+    return current;
+}
---
+++ SmoothNoise.h
@@ -0,0 +1,21 @@
+#ifndef nmap_h
+#define nmap_h
+
+#include "Arduino.h"
+#include "math.h"
+
+float cubic(float n);
+float cubic_interpolate(float n, float a, float b, float c, float d);
+
+class SmoothNoise {
+    float last;
+    float next;
+    int steps;
+    int steps_from_last;
+
+    public:
+        SmoothNoise(int num_steps);
+        float noise(void);
+};
+
+#endif
